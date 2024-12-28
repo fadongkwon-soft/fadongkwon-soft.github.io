8809ece2b3a1076e12b5ba45cace2e9e83bcbbef
fadongkwon-soft <admin@fadongkwon.com>
2024-12-28 17:28:20 +0900

    avatar

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735348425',resources: [
+    cacheName: 'chirpy-1735374374',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
